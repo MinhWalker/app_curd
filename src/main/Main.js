a4f7ceedcfe2d8ed5dd21904542b689188cd85da
minhwalker <minhnh421999@gmail.com>
2021-02-22 18:13:20 +0700

    finish

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,26 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Main = () => {
+    const [products, setproducts] = useState([])
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:8000/api/products')
+
+                const data = await response.json()
+
+                setproducts(data)
+            }
+        )()
+    }, [])
+
+    const like = async (id) => {
+        await fetch(`http://localhost:8001/api/products/${id}/like`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'Application/json' }
+        })
+
+        setproducts(products.map(
+            (p => {
+                if (p.id === id) {
+                    p.likes++
+                }
+
+                return p
+            })
+        ))
+    }
+
     return (
         <main>
             <div class="album py-5 bg-light">
                 <div class="container">
                     <div class="row ">
-                        <div class="col-md-4">
-                            <div class="card shadow-sm">
-
-                                <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="btn-group">
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                        {products.map(
+                            p => {
+                                return (
+                                    <div class="col-md-4">
+                                        <div class="card shadow-sm">
+                                            <img src={p.image}></img>
+                                            <div class="card-body">
+                                                <p class="card-text">{p.title}</p>
+                                                <div class="d-flex justify-content-between align-items-center">
+                                                    <div class="btn-group">
+                                                        <button type="button"
+                                                            class="btn btn-sm btn-outline-secondary"
+                                                            onClick={() => like(p.id)}
+                                                        >
+                                                            Like
+                                                        </button>
+                                                    </div>
+                                                    <small class="text-muted">{p.likes} Likes</small>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <small class="text-muted">9 mins</small>
                                     </div>
-                                </div>
-                            </div>
-                        </div>
+                                )
+                            }
+                        )}
                     </div>
                 </div>
             </div>
